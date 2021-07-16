2de48b1ab3c2bfc371786bef5060d1022752bce1
Luis <luisechvz@gmail.com>
2021-07-16 17:14:02 -0500

    Primer ejercicio sobre el promedio

---
+++ primeraP.py
@@ -0,0 +1,10 @@
+"""
+Calcular calificacion del alumno si
+    El primer examen cuenta un 40% y el segundo un 60%
+"""
+
+examen_uno = 10
+examen_dos = 8
+
+promedio = ((examen_uno*.4)+(examen_dos*.6))
+print("Tu promedio es de " + str(promedio))
